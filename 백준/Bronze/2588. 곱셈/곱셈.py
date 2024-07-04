d16ce54522504cfe26463cf434bd3f8d4629a08f
karnelll <165611407+karnelll@users.noreply.github.com>
2024-07-04 12:33:15 +0900

    Update 곱셈.py

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -6,3 +6,17 @@ print(ADD_list[1])
 print(ADD_list[0])
 final_result = ADD_list[2] + ADD_list[1]*10 + ADD_list[0]*100
 print(final_result)
+
+'''A = int(input())
+
+B = input()
+
+ 
+
+print(A*int(B[2]))
+
+print(A*int(B[1]))
+
+print(A*int(B[0]))
+
+print(A*int(B))'''
