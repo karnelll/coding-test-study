dcac5807e1d4b19634c77e2de95ad5ca1904732c
karnelll <165611407+karnelll@users.noreply.github.com>
2024-07-28 01:51:30 +0900

    [level 0] Title: 두 수의 합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.py
@@ -0,0 +1,13 @@
+def solution(num1, num2):
+    result = num1 + num2
+    return result
+    
+from solution import solution
+
+num1, num2 = 2, 3
+result = solution(num1, num2)
+print(result)
+
+num1, num2 = 100, 2
+result = solution(num1, num2)
+print(result)
