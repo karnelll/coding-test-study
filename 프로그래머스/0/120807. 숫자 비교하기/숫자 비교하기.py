5003c7be0dc925923cedaf954d0dfd058e36ea37
karnelll <165611407+karnelll@users.noreply.github.com>
2024-08-02 12:23:58 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.py
@@ -0,0 +1,10 @@
+def solution(num1, num2):
+    if num1 == num2:
+        result = 1
+        return result
+    
+    else:
+        result = -1
+        return result
+    answer = 0
+    return answer
